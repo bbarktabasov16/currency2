750f70e2fdf1f87a0a48dc222043d4f619e32031
Bekzat Baryktabasov <bbarktabasov16@gmail.com>
2024-06-10 22:08:50 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import "./App.css"
 
 function App() {
+  const [amount, setAmount] = useState(0);
+  const [currencyValue, setCurrencyValue] = useState("USD");
+  const [result, setResult] = useState(0);
+
+  const usd = 87.01;
+  const rub = 0.98;
+  const rial = 23.20;
+  const kz = 0.19;
+
+  const handleCurrencyChange = (e) => {
+    setCurrencyValue(e.target.value);
+  }
+
+  const handleAmountChange = (e) => {
+    setAmount(e.target.value);
+  }
+
+  const sumCurrent = () => {
+    let rate = 0;
+    switch (currencyValue) {
+      case "USD":
+        rate = usd;
+        break;
+      case "RUB":
+        rate = rub;
+        break;
+      case "RIAL":
+        rate = rial;
+        break;
+      case "KZ":
+        rate = kz;
+        break;
+      default:
+        rate = 1;
+    }
+    setResult(amount * rate);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <select id="currency" onChange={handleCurrencyChange}>
+        <option value="USD">USD</option>
+        <option value="RUB">RUB</option>
+        <option value="RIAL">RIAL</option>
+        <option value="KZ">KZ</option>
+      </select>
+      <form>
+        <label>Акча алмаштыруу</label>
+        <br />
+        <input 
+          type="number" 
+          id="mount" 
+          value={amount} 
+          onChange={handleAmountChange} 
+        />
+      </form>
+      <div>
+        <button type="button" onClick={sumCurrent}>Алмаштыруу</button>
+      </div>
+      <h1 id="result">{result ? result : "Жыйынтык"}</h1>
     </div>
   );
 }
